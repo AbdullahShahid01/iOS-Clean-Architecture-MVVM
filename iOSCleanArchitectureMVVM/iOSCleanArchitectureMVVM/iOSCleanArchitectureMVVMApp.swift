b92ffb34b21906c9c51543968149e5b17097b1fa
Abdullah Shahid <abdullah.shahid@evampsaanga.com>
2025-01-29 22:18:33 +0500

    iOS Project Created

---
+++ iOSCleanArchitectureMVVM/iOSCleanArchitectureMVVM/iOSCleanArchitectureMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSCleanArchitectureMVVMApp.swift
+//  iOSCleanArchitectureMVVM
+//
+//  Created by Abdullah Shahid on 29/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct iOSCleanArchitectureMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
